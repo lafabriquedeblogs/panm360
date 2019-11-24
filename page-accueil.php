4efea4036882da9798e6008f1d3f5ec2807dedc5
LFDB <philippe@lafabriquedeblogs.com>
2019-11-24 17:13:57 -0500

    boutons

--- page-accueil.php
+++ page-accueil.php
@@ -184,9 +184,7 @@ get_header();
 		<div class="panm360-infolettre">
 			<img src="<?php echo get_template_directory_uri(  );?>/assets/img/panm360.png" width="146" height="49"/>
 			<span>Infolettre PAN.M360</span>
-			<form id="infolettre">
-			
-			</form>
+			<?php echo do_shortcode( '[gravityforms id="1" title="false" description="false" ajax="true"]' ); ?>
 		</div>
 		
 	</section>
