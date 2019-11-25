e780e8102f21e37efd13e1585c4270367df83ee6
LFDB <philippe@lafabriquedeblogs.com>
2019-11-24 20:26:29 -0500

    debug shorizontal scroll firefox

--- page-accueil.php
+++ page-accueil.php
@@ -183,7 +183,7 @@ get_header();
 		
 		<div class="panm360-infolettre">
 			<img src="<?php echo get_template_directory_uri(  );?>/assets/img/panm360.png" width="146" height="49"/>
-			<span>Infolettre PAN.M360</span>
+			<span class="infolettre-titre">Infolettre PAN.M360</span>
 			<?php echo do_shortcode( '[gravityforms id="1" title="false" description="false" ajax="true"]' ); ?>
 		</div>
 		
