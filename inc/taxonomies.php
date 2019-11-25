70ccd896ddf2f7183e5c68057d75ecf08492eb09
fabrice <fabrice@lafabriquedeblogs.com>
2019-11-25 15:52:37 -0500

    update

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -35,7 +35,7 @@ function panm360_taxonomies() {
 		'show_tagcloud'              => true,
 		'show_in_rest'               => true,
 	);
-	register_taxonomy( 'generale', array(  'post','artiste','records' ), $args_generale );
+	register_taxonomy( 'generale', array(  'post','artiste','records' , 'concerts' ), $args_generale );
 	
 	$labels_genre = array(
 		'name'                       => _x( 'Genres', 'Taxonomy General Name', 'panm360' ),
@@ -69,7 +69,7 @@ function panm360_taxonomies() {
 		'show_tagcloud'              => true,
 		'show_in_rest'               => true,
 	);
-	register_taxonomy( 'genre', array(  'post','artiste','records' ), $args_genre );
+	register_taxonomy( 'genre', array(  'post','artiste','records' , 'concerts' ), $args_genre );
 
 
 	$annee_album_labels = array(
@@ -104,7 +104,7 @@ function panm360_taxonomies() {
 		'show_tagcloud'              => true,
 		'show_in_rest'               => true,
 	);
-	register_taxonomy( 'annee', array(  'post','artiste','records' ), $args_annee_album );
+	register_taxonomy( 'annee', array(  'post','artiste','records' , 'concerts' ), $args_annee_album );
 
 	$pays_labels = array(
 		'name'                       => _x( 'Pays', 'Taxonomy General Name', 'panm360' ),
@@ -138,7 +138,7 @@ function panm360_taxonomies() {
 		'show_tagcloud'              => true,
 		'show_in_rest'               => true,
 	);
-	register_taxonomy( 'pays', array( 'post','artiste','records' ), $args_pays );
+	register_taxonomy( 'pays', array( 'post','artiste','records' , 'concerts' ), $args_pays );
 	
 }
 add_action( 'init', 'panm360_taxonomies', 0 );
--- page-accueil.php
+++ page-accueil.php
@@ -178,15 +178,8 @@ get_header();
 	
 	<?php get_template_part( '/template-parts/publicites/publicite', 'banniere' ); ?>
 	
+	<?php get_template_part( '/template-parts/modules/element', 'infolettre' ); ?>
 	
-	<section class="section infolettre">
-		
-		<div class="panm360-infolettre">
-			<img src="<?php echo get_template_directory_uri(  );?>/assets/img/panm360.png" width="146" height="49"/>
-			<span class="infolettre-titre">Infolettre PAN.M360</span>
-			<?php echo do_shortcode( '[gravityforms id="1" title="false" description="false" ajax="true"]' ); ?>
-		</div>
-		
-	</section>
+
 <?php
 get_footer();
