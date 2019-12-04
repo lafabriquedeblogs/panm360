2739fe5319a32d5c5718512cdfb0d01956a6c040
LFDB <philippe@lafabriquedeblogs.com>
2019-12-03 19:18:31 -0500

    menu

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 
 		</div><!-- branding-navigation -->
 		
-		<?php get_template_part( 'template-parts/menus/menu','submenu-desktop' ); ?>
+		<?php //get_template_part( 'template-parts/menus/menu','submenu-desktop' ); ?>
 
 	</header><!-- #masthead -->
 	
