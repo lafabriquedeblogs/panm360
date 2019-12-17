34cabece60935b1533824cdcea605290cd002204
LFDB <philippe@lafabriquedeblogs.com>
2019-12-17 16:55:54 -0500

    yeah

--- template-parts/modules/element-infolettre.php
+++ template-parts/modules/element-infolettre.php
@@ -1,9 +1,14 @@
-	<section class="section infolettre">
+	<?php
 		
+		//$infolettre_page_id = 5573;
+		$infolettre_page_id = apply_filters( 'wpml_object_id', 4508, 'page' );
+		$infolettre_page_permalink = get_permalink( $infolettre_page_id );
+
+	?>
+	<section class="section infolettre">	
 		<div class="panm360-infolettre">
+			<span class="infolettre-titre"><?php _e('Abonnez-vous à l\'infolettre','panm360'); ?></span>
 			<svg class="icon"><use xlink:href="#panm360"></use></svg>
-			<span class="infolettre-titre">Infolettre PAN.M360</span>
-			<?php echo do_shortcode( '[gravityforms id="1" title="false" description="false" ajax="true"]' ); ?>
+			<button onclick="window.location.href = '<?php echo $infolettre_page_permalink;?>'"><?php _e('M\'abonner','panm360'); ?></button>
 		</div>
-		
 	</section>
