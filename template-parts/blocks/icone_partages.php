d9a88d9b6995c0befa438e62abaf07bb24f1385e
LFDB <philippe@lafabriquedeblogs.com>
2019-12-17 21:18:13 -0500

    -

--- template-parts/blocks/icone_partages.php
+++ template-parts/blocks/icone_partages.php
@@ -1,12 +1,5 @@
 <?php
-/*
-	array (size=2)
-      'icone_reseau_social' => 
-        array (size=2)
-          'ID' => string 'facebook' (length=8)
-          '_file_url' => string 'http://fabrice-2.local:5757/wp-content/themes/panm360/assets/img/panm360_sprite.svg' (length=72)
-      'url' => string 'https://facebook.com'	
-*/
+
 add_action( 'acf/init', 'register_panm360_icones_partage');
 
 function register_panm360_icones_partage() {
@@ -39,37 +32,14 @@ function icones_partage_render_callback( $block, $content = '', $is_preview = fa
 	    $className .= ' align' . $block['align'];
 	}
 	
-	$rows_icons = get_field("reseau_social",'options');
-	
-	//%permalink%
-	
-	
-	
-	if( is_admin() )
-		include_once( get_template_directory() . "/assets/img/panm360_sprite.svg");
-	
 	?>
 		
        <!-- Go to www.addthis.com/dashboard to customize your tools -->
        <div class="addthis_inline_share_toolbox"></div>
        
-    <?php /* ?>        
-		<ul id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-			
-				<?php foreach( $rows_icons as $icon ){
-					
-					echo '<pre>';
-						var_dump($icon);
-					echo '</pre>';
-				?>
-				
-						<li class="reseau-partage">
-							<a href="" id="<?php echo $icon['icone_reseau_social']['ID'];?>">
-								<svg class="icon"><use xlink:href="#<?php echo $icon['icone_reseau_social']['ID'];?>"></use></svg>
-							</a>
-						</li>
-				<?php }	?>
-			
-		</ul>
-	<?php */ ?>		
+       
+    <?php if( is_admin() ): ?>
+    
+    	<img src="<?php echo get_template_directory_uri(  );?>/assets/img/socials_admin_block.jpg" width="121px" height="57px"/>
+    <?php endif; ?>		
 <?php } ?>
