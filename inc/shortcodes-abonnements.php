45289e20634c199108c4325b009d86499c740110
LFDB <philippe@lafabriquedeblogs.com>
2019-12-19 15:14:40 -0500

    panm360-abonnement

--- inc/shortcodes-abonnements.php
+++ inc/shortcodes-abonnements.php
@@ -16,30 +16,30 @@ function abos_panm_st( $atts ) {
 		?>
 			<div class="abo-bloc abo-bloc-gratuit">
 				<div>
-					<span class="abo-prix"><?php _e('Gratuit','panm360'); ?></span>
+					<span class="abo-prix"><?php _e('Gratuit','panm360-abonnement'); ?></span>
 				</div>
 				<div>
-					<span class="abo-asset"><?php _e('L’abonnement gratuit permet','panm360'); ?> <span class="cap"><?php _e('CHAQUE MOIS','panm360'); ?></span>:</span>
+					<span class="abo-asset"><?php _e('L’abonnement gratuit permet','panm360-abonnement'); ?> <span class="cap"><?php _e('CHAQUE MOIS','panm360-abonnement'); ?></span>:</span>
 				</div>
 				<ul id="abonnement-gratuit" class="ul-abonnement">
-					<li class="item-titre"><strong><?php _e('LIRE','panm360'); ?></strong> </li>
-					<li class="item-abo">- <?php _e('Le calendrier quotidien des concerts','panm360'); ?></li>
-					<li class="item-abo">- <strong>2</strong> <?php _e('critiques d’albums','panm360'); ?></li>
-					<li class="item-abo">- <strong>2</strong> <?php _e('comptes rendus de concerts','panm360'); ?></li>
-					<li class="item-abo">- <strong>1</strong> <?php _e('interview','panm360'); ?></li>
-					<li class="item-abo">- <strong>1</strong> <?php _e('chronique','panm360'); ?></li>
-					<li class="item-abo unavailable">- <?php _e('reportage','panm360'); ?></li>
-					<li class="item-abo">- <strong>1</strong> <?php _e('conversation entre artistes ou spécialistes de différents genres sur un sujet ayant trait à la musique','panm360'); ?></li>
-					<li class="item-abo unavailable">- <?php _e('Texte de vulgarisation','panm360'); ?></li>
-					<li class="item-abo unavailable">- <?php _e('archives','panm360'); ?></li>
-					<li class="item-titre"><strong><?php _e('ÉCOUTER','panm360'); ?></strong></li>
-					<li class="item-abo">- <strong>1</strong> <?php _e('liste d’écoute','panm360'); ?></li>
-					<li class="item-abo">- <strong>1</strong> <?php _e('balado','panm360'); ?></li>
-					<li class="item-titre"><strong><?php _e('VISIONNER','panm360'); ?></strong></li>
-					<li class="item-abo">- <strong>1</strong> <?php _e('interview-performance PAN.M@SAT','panm360'); ?></li>
+					<li class="item-titre"><strong><?php _e('LIRE','panm360-abonnement'); ?></strong> </li>
+					<li class="item-abo">- <?php _e('Le calendrier quotidien des concerts','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong>2</strong> <?php _e('critiques d’albums','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong>2</strong> <?php _e('comptes rendus de concerts','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong>1</strong> <?php _e('interview','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong>1</strong> <?php _e('chronique','panm360-abonnement'); ?></li>
+					<li class="item-abo unavailable">- <?php _e('reportage','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong>1</strong> <?php _e('conversation entre artistes ou spécialistes de différents genres sur un sujet ayant trait à la musique','panm360-abonnement'); ?></li>
+					<li class="item-abo unavailable">- <?php _e('Texte de vulgarisation','panm360-abonnement'); ?></li>
+					<li class="item-abo unavailable">- <?php _e('archives','panm360-abonnement'); ?></li>
+					<li class="item-titre"><strong><?php _e('ÉCOUTER','panm360-abonnement'); ?></strong></li>
+					<li class="item-abo">- <strong>1</strong> <?php _e('liste d’écoute','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong>1</strong> <?php _e('balado','panm360-abonnement'); ?></li>
+					<li class="item-titre"><strong><?php _e('VISIONNER','panm360-abonnement'); ?></strong></li>
+					<li class="item-abo">- <strong>1</strong> <?php _e('interview-performance PAN.M@SAT','panm360-abonnement'); ?></li>
 				</ul>
 				<div>
-					<button class=""><?php _e('Je m\'inscris','panm360'); ?></button>
+					<button class=""><?php _e('Je m\'inscris','panm360-abonnement'); ?></button>
 				</div>
 			</div><!-- abo-bloc abo-bloc-gratuit -->		
 		<?php
@@ -49,30 +49,30 @@ function abos_panm_st( $atts ) {
 		?>
 			<div class="abo-bloc abo-bloc-mensuel-1">
 				<div>
-					<span class="abo-prix">3<span class="cents">.50$</span> <span class="frequence">/<?php _e('par mois','panm360'); ?></span></span>
+					<span class="abo-prix">3<span class="cents">.50$</span> <span class="frequence">/<?php _e('par mois','panm360-abonnement'); ?></span></span>
 				</div>
 				<div>
-					<span class="abo-asset"><?php _e('L’abonnement payant permet','panm360'); ?> <span class="cap"><?php _e('CHAQUE SEMAINE','panm360'); ?></span>:</span>
+					<span class="abo-asset"><?php _e('L’abonnement payant permet','panm360-abonnement'); ?> <span class="cap"><?php _e('CHAQUE SEMAINE','panm360-abonnement'); ?></span>:</span>
 				</div>
 				<ul id="abonnement-mensuel-1" class="ul-abonnement">
-					<li class="item-titre"><strong><?php _e('LIRE','panm360'); ?></strong> </li>
-					<li class="item-abo">- <?php _e('Le calendrier quotidien des concerts','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Toutes','panm360'); ?></strong> <?php _e('les critiques d’albums','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Tous','panm360'); ?></strong> <?php _e('les comptes rendus de concerts','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Toutes','panm360'); ?></strong> <?php _e('les interviews','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Toutes','panm360'); ?></strong> <?php _e('les chroniques','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Tous','panm360'); ?></strong> <?php _e('nos reportage','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Toutes','panm360'); ?></strong>  <?php _e('les conversations entre artistes ou spécialistes de différents genres sur un sujet ayant trait à la musique','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Tous','panm360'); ?></strong> <?php _e('les textes de vulgarisation','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Toutes','panm360'); ?></strong> <?php _e('nos archives','panm360'); ?></li>
-					<li class="item-titre"><strong><?php _e('ÉCOUTER','panm360'); ?></strong></li>
-					<li class="item-abo">- <strong><?php _e('Toutes','panm360'); ?></strong> <?php _e('nos listes d’écoute','panm360'); ?></li>
-					<li class="item-abo">- <strong><?php _e('Tous','panm360'); ?></strong> <?php _e('nos balados','panm360'); ?></li>
-					<li class="item-titre"><strong><?php _e('VISIONNER','panm360'); ?></strong></li>
-					<li class="item-abo">- <strong><?php _e('Toutes','panm360'); ?></strong> <?php _e('nos interviews-performances PAN.M@SAT','panm360'); ?></li>
+					<li class="item-titre"><strong><?php _e('LIRE','panm360-abonnement'); ?></strong> </li>
+					<li class="item-abo">- <?php _e('Le calendrier quotidien des concerts','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Toutes','panm360-abonnement'); ?></strong> <?php _e('les critiques d’albums','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Tous','panm360-abonnement'); ?></strong> <?php _e('les comptes rendus de concerts','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Toutes','panm360-abonnement'); ?></strong> <?php _e('les interviews','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Toutes','panm360-abonnement'); ?></strong> <?php _e('les chroniques','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Tous','panm360-abonnement'); ?></strong> <?php _e('nos reportage','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Toutes','panm360-abonnement'); ?></strong>  <?php _e('les conversations entre artistes ou spécialistes de différents genres sur un sujet ayant trait à la musique','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Tous','panm360-abonnement'); ?></strong> <?php _e('les textes de vulgarisation','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Toutes','panm360-abonnement'); ?></strong> <?php _e('nos archives','panm360-abonnement'); ?></li>
+					<li class="item-titre"><strong><?php _e('ÉCOUTER','panm360-abonnement'); ?></strong></li>
+					<li class="item-abo">- <strong><?php _e('Toutes','panm360-abonnement'); ?></strong> <?php _e('nos listes d’écoute','panm360-abonnement'); ?></li>
+					<li class="item-abo">- <strong><?php _e('Tous','panm360-abonnement'); ?></strong> <?php _e('nos balados','panm360-abonnement'); ?></li>
+					<li class="item-titre"><strong><?php _e('VISIONNER','panm360-abonnement'); ?></strong></li>
+					<li class="item-abo">- <strong><?php _e('Toutes','panm360-abonnement'); ?></strong> <?php _e('nos interviews-performances PAN.M@SAT','panm360-abonnement'); ?></li>
 				</ul>
 				<div>
-					<button class=""><?php _e('Je m\'abonne','panm360'); ?></button>
+					<button class=""><?php _e('Je m\'abonne','panm360-abonnement'); ?></button>
 				</div>
 			</div><!-- abo-bloc abo-bloc-gratuit -->		
 		<?php
