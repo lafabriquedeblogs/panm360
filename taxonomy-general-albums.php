594f2cb7da3df7dc9971dc0b3362abb6d22ba927
LFDB <philippe@lafabriquedeblogs.com>
2020-01-16 16:53:31 -0500

    m

---
+++ taxonomy-general-albums.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<article>
+			<header class="section">
+				<h1 clas="entry-title"><span class="light"><?php echo single_cat_title( '', false ); ?></span></h1>
+			</header>
+			<section class="section">
+				<div class="section-inner">
+				
+					<div class="section-content-c">
+						<ul class="section-content--has-6-columns">
+								<?php
+									
+									while ( have_posts() ) :
+										the_post();
+										$posts_count = '';
+										include( locate_template( '/template-parts/modules/element-album.php', false, false ) );
+									endwhile;
+																
+								?>
+								<li id="pages-liste-navigation" >
+									<?php
+											the_posts_pagination( array(
+											'mid_size'  => 4,
+											'prev_text' => __( 'Précédente', 'panm360' ),
+											'next_text' => __( 'Suivante', 'panm360' ),
+										) );
+									?>
+								</li>
+							</ul><!-- section-content--has-6-columns -->
+						
+					</div><!-- section-content-c -->
+					
+				</div><!-- section-inner -->
+			</section><!-- section -->
+			
+			</article>
+			
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
