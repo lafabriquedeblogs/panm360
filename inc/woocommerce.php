16bb3eaf3e566fb983b8a6a51812073b05a977dd
fabrice <fabrice@lafabriquedeblogs.com>
2020-01-23 12:35:30 -0500

    filtre woocommerce

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,6 +1,6 @@
 <?php
 	
 	function remove_wc_memberships_the_restricted_content_message(){
-		return false;
+		return '';
 	}
-	add_filter('wc_memberships_product_viewing_restricted_message' , 'remove_wc_memberships_the_restricted_content_message');
+	add_filter('wc_memberships_content_restricted_message' , 'remove_wc_memberships_the_restricted_content_message');
