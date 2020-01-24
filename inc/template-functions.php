ce0604d287cd6541b773cc1c2c8452de2445366d
fabrice <fabrice@lafabriquedeblogs.com>
2020-01-24 16:27:21 -0500

    affichage content

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -103,11 +103,6 @@ function get_genre_parents( $post_id , $link = false){
 	            $link_term = get_term_link( $parent->term_id, 'genre' );
 	            $genres[] = '<a href="'.$link_term.'">'.$parent->name.'</a>';
             }
-/*
-	        echo '<pre>';
-	        var_dump($parent->name);
-	        echo '</pre>';
-*/
         }
 	}
 	
@@ -339,4 +334,50 @@ function get_liste_filtre_artiste(){
 		$liste[] = '<a href="'.$lien_album.'">'.$nom.'</a>';
 	}
 	return implode(" • ", $liste);
+}
+
+add_filter( 'the_content', 'filter_the_content_in_the_main_loop' );
+ 
+function filter_the_content_in_the_main_loop( $content ) {
+
+		$user_id = get_current_user_id();
+
+		if( $user_id == 0 && !is_page() ){
+			
+			global $post;
+			
+			$content_strip = strip_tags($post->post_content);
+			$content_total_length = str_word_count($content_strip);
+			$words = $content_total_length / 2 ;
+			$more = '…';
+			 
+			$excerpt = wp_trim_words( $content, $words, $more );
+			echo $excerpt;
+			
+		} else {
+			
+			$args = array( 
+				'status' => array( 'active', 'complimentary', 'pending','free_trial' ),
+			);  
+			
+			$active_memberships = wc_memberships_get_user_memberships( $user_id, $args );
+			$active_subscriptions = wcs_get_users_subscriptions($user_id);
+			
+			$active_memberships_names = array();
+			
+			foreach( $active_memberships as $membership){
+				$active_memberships_names[] = $membership->plan->name;
+			}
+			
+			if( in_array('Premium', $active_memberships_names ) ){
+				return $content;
+			}
+			
+			$viewed_interviews = get_user_meta( $user_id , 'viewed_interviews', true );
+			$viewed_albums = get_user_meta( $user_id , 'viewed_albums', true );
+			$viewed_review = get_user_meta( $user_id , 'viewed_review', true );
+			$viewed_chronique = get_user_meta( $user_id , 'viewed_chronique', true );
+			
+		}
+
 }
--- inc/woocommerce/woocommerce.php
+++ inc/woocommerce/woocommerce.php
@@ -5,11 +5,3 @@ function remove_wc_memberships_the_restricted_content_message($message, $args){
 	return $message;
 }
 add_filter('wc_memberships_the_restricted_content' , 'remove_wc_memberships_the_restricted_content_message',10,2);
-
-
-function sv_wc_memberships_restricted_excerpt_length() {
-	return 20;
-}
-add_filter( 'wc_memberships_restricted_excerpt_length', 'sv_wc_memberships_restricted_excerpt_length' );
-
-require get_template_directory() . '/inc/woocommerce/custom_excerpt.php';
