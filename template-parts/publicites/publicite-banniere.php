c6fbcad78e0f1831825de4465d0b1132ce3736ad
LFDB <philippe@lafabriquedeblogs.com>
2020-02-13 22:14:50 -0500

    m

--- template-parts/publicites/publicite-banniere.php
+++ template-parts/publicites/publicite-banniere.php
@@ -1,5 +1,5 @@
 <section class="publicite publicite-banniere">
- <ins class="adsbygoogle"
+<ins class="adsbygoogle"
      style="display:inline-block;width:728px;height:90px;margin:50px auto;"
      data-ad-client="ca-pub-7945905951488029"
      data-ad-slot="4875913156"></ins>
