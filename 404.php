58daea058d39fe9f200f8876b3978dd285972ebb
LFDB <philippe@lafabriquedeblogs.com>
2020-02-14 08:53:05 -0500

    m

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
+			<section class="error-404 not-found section">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'panm360' ); ?></h1>
 				</header><!-- .page-header -->
@@ -21,6 +21,7 @@ get_header();
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'panm360' ); ?></p>
 				</div><!-- .page-content -->
+			
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
