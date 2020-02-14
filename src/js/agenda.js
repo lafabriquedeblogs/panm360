b6c08b72e3019928dc9b622711dd33fffb30f979
LFDB <philippe@lafabriquedeblogs.com>
2020-02-14 07:44:05 -0500

    m

--- src/js/agenda.js
+++ src/js/agenda.js
@@ -128,6 +128,7 @@ export default function ajax_search_agenda(){
 			autoplay: true,
 			autoplaySpeed: 4000,
 			adaptiveHeight: true,
+			lazyLoad: 'ondemand',
 			prevArrow: '<div class="slick-prev-arrow"><svg class="icone"><use xlink:href="#slider-left-arrow"></use></svg></div>',
 			nextArrow: '<div class="slick-next-arrow"><svg class="icone"><use xlink:href="#slider-right-arrow"></use></svg></div>',
 		});
