5fb5991751fb5ffe90d853f211e3668b5b5ca5ea
LFDB <philippe@lafabriquedeblogs.com>
2020-02-14 07:30:27 -0500

    m

--- template-parts/publicites/publicite-banniere.php
+++ template-parts/publicites/publicite-banniere.php
@@ -1,6 +1,6 @@
 <section class="publicite publicite-banniere">
 <ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px;margin:50px auto;"
+     style="display:inline-block;width:728px;height:90px;"
      data-ad-client="ca-pub-7945905951488029"
      data-ad-slot="4875913156"></ins>
 <script>
