907f37dea313074e87c0eb8d6940b55cb735b1be
LFDB <philippe@lafabriquedeblogs.com>
2020-02-25 17:29:35 -0500

    m

--- inc/abonnements/woocommerce.php
+++ inc/abonnements/woocommerce.php
@@ -165,11 +165,14 @@ function sv_wc_memberships_limit_membership_count( $grant_access, $args ) {
 	// get all active memberships for the purchaser
 	// you can remove any of these if you don't want to allow multiples
 	// ie you may not want to count complimentary memberships
+/*
 	$statuses = array(
 		'status' => array( 'active', 'complimentary', 'pending', 'free_trial' ),
 	);
-
+	
 	$active_memberships = wc_memberships_get_user_memberships( $args['user_id'], $statuses );
+*/
+	$active_memberships = wc_memberships_get_user_memberships( $args['user_id'] );
 	
 	// if there are any active memberships returned, do not grant access from purchase
 	if ( ! empty( $active_memberships ) ) {
@@ -178,5 +181,5 @@ function sv_wc_memberships_limit_membership_count( $grant_access, $args ) {
 
 	return $grant_access;
 }
-//add_filter( 'wc_memberships_grant_access_from_new_purchase', 'sv_wc_memberships_limit_membership_count', 1, 2 );
+add_filter( 'wc_memberships_grant_access_from_new_purchase', 'sv_wc_memberships_limit_membership_count', 1, 2 );
 
