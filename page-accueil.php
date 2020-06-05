e719752f5797089cbc6b9581a042232570df9e20
LFDB <philippe@lafabriquedeblogs.com>
2020-06-05 07:55:25 -0400

    publicité analekta

--- page-accueil.php
+++ page-accueil.php
@@ -10,6 +10,8 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			
+			<?php get_template_part( '/template-parts/publicites/publicite', 'banniere-analekta' ); ?>
 						
 			<?php the_content();	?>
 
